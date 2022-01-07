04534b668560876329e9c53152a5fb7ec2b61211
Keallu <45124091+keallu@users.noreply.github.com>
2022-01-07 11:52:34 +0100

    Version 1.5.

--- BulldozeIt/ModConfig.cs
+++ BulldozeIt/ModConfig.cs
@@ -4,10 +4,10 @@
     public class ModConfig
     {
         public bool ConfigUpdated { get; set; }
-        public bool AbandonedBuildings { get; set; }
-        public bool BurnedDownBuildings { get; set; }
-        public bool CollapsedBuildings { get; set; }
-        public bool FloodedBuildings { get; set; }
+        public bool AbandonedBuildings { get; set; } = true;
+        public bool BurnedDownBuildings { get; set; } = true;
+        public bool CollapsedBuildings { get; set; } = true;
+        public bool FloodedBuildings { get; set; } = true;
         public int Interval { get; set; } = 1;
         public int MaxBuildingsPerInterval { get; set; } = 32;
         public bool PreserveHistoricalBuildings { get; set; } = true;
